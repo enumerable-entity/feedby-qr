c715d75148a003e6da6cd5d497aa9d135c0c4848
Enumerable-entity <alfred.dc.alpha@gmail.com>
2023-05-31 20:29:00 +0200

    Init

---
+++ src/main/java/com/enumerableentity/feedby/FeedbyApplication.java
@@ -0,0 +1,13 @@
+package com.enumerableentity.feedby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbyApplication.class, args);
+    }
+
+}
